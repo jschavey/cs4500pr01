87fbbc47e2b78e5e7f1168b653419c3ec020029b
John Schavey <jschavey@me.com>
2017-10-16 21:50:16 +0000

    refactor tests

--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -1,14 +1,12 @@
 require_relative '../code/machine'
 
-RSpec.describe Machine, "#start_state" do
-    it 'is always 0' do
+RSpec.describe Machine do
+    it 'has start state 0' do
         machine = Machine.new([], [])
         machine.instance_variable_set(:@start_state, 42)
         expect(machine.isValid?).to be_falsey
     end
-end
-
-RSpec.describe Machine, "#isValid?" do
+    
     it 'has at most 256 states' do
         too_many_states = (1...300).to_a
         machine = Machine.new(too_many_states,[])
